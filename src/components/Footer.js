dd73518da90c15e3d8ad3948a5360e07a056ad59
warmeta <warmeta@gmail.com>
2020-05-28 21:14:44 +0100

    fix: add container to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,24 +2,26 @@ import React from 'react';
 import { FaHeart } from 'react-icons/fa';
 
 // components
-import { Footer as BFooter, Content, Level } from 'react-bulma-components';
+import { Footer as BFooter, Container, Content, Level } from 'react-bulma-components';
 
 const Footer = () => {
   return (
     <BFooter renderAs="footer" color="secondary" className="has-background-primary">
-      <Content className="has-text-right">
-        <Level>
-          <Level.Side align="left" />
-          <Level.Side align="right">
-            <Level.Item>
-              <strong>Made with Gatsby and Bulma</strong>
-            </Level.Item>
-            <Level.Item>
-              <FaHeart style={{ marginLeft: 10 }} />
-            </Level.Item>
-          </Level.Side>
-        </Level>
-      </Content>
+      <Container>
+        <Content className="has-text-right">
+          <Level>
+            <Level.Side align="left" />
+            <Level.Side align="right">
+              <Level.Item>
+                <strong>Made with Gatsby and Bulma</strong>
+              </Level.Item>
+              <Level.Item>
+                <FaHeart style={{ marginLeft: 10 }} />
+              </Level.Item>
+            </Level.Side>
+          </Level>
+        </Content>
+      </Container>
     </BFooter>
   );
 };
